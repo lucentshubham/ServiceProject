0d51b4d51a657ccc45e94ccb00579571ebc0684d
chaudharykapil <chaudharykapil8279@gmail.com>
2021-06-28 16:10:45 +0530

    add database

--- Database/databases.py
+++ Database/databases.py
@@ -1,45 +1,50 @@
-from mainserver import db
-class ServiceProvider(db.Model):
+from flask_sqlalchemy import SQLAlchemy
+maindb = SQLAlchemy()
+class ServiceProvider(maindb.Model):
     __tablename__ = "serviceprovider"
-    sno = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(50),nullable=False)
-    email = db.Column(db.String(50),nullable = False)
-    profession = db.Column(db.String(50),nullable = False)
-    category = db.Column(db.String(50),nullable = False)
-    city = db.Column(db.String(30),nullable = False)
-    phone = db.Column(db.String(13),nullable = True)
-    speciality = db.Column(db.String(50),nullable = True)
-    qualification = db.Column(db.Text,nullable=True)
-    images = db.Column(db.PickleType,nullable = True)
-    address = db.Column(db.String(100),nullable = False)
-    open_close_time = db.Column(db.DateTime,nullable = True)
-    experience = db.Column(db.Float,nullable=True)
+    sno = maindb.Column(maindb.Integer,primary_key = True)
+    name = maindb.Column(maindb.String(50),nullable=False)
+    email = maindb.Column(maindb.String(50),nullable = False)
+    password = maindb.Column(maindb.String(50),nullable = False)
+    profession = maindb.Column(maindb.String(50),nullable = False)
+    category = maindb.Column(maindb.String(50),nullable = False)
+    city = maindb.Column(maindb.String(30),nullable = True)
+    phone = maindb.Column(maindb.String(13),nullable = False)
+    speciality = maindb.Column(maindb.String(50),nullable = True)
+    qualification = maindb.Column(maindb.Text,nullable=True)
+    images = maindb.Column(maindb.PickleType,nullable = True)
+    address = maindb.Column(maindb.String(100),nullable = True)
+    open_close_time = maindb.Column(maindb.DateTime,nullable = True)
+    experience = maindb.Column(maindb.Float,nullable=True)
 
-class Ratings(db.Model):
+class Ratings(maindb.Model):
     __tablename__ = "rating"
-    sno = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(50),nullable = False)
-    no_of_rating = db.Column(db.Integer,nullable = False)
-    rating_list = db.Column(db.PickleType,nullable = False)
+    sno = maindb.Column(maindb.Integer,primary_key = True)
+    name = maindb.Column(maindb.String(50),nullable = False)
+    no_of_rating = maindb.Column(maindb.Integer,nullable = False)
+    rating_list = maindb.Column(maindb.PickleType,nullable = False)
 
 
-class Reviews(db.Model):
+class Reviews(maindb.Model):
     __tablename__ = "reviews"
-    sno = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(50),nullable = False)
-    no_of_review = db.Column(db.Integer,nullable = False)
-    review_list = db.Column(db.PickleType,nullable = False)    
+    sno = maindb.Column(maindb.Integer,primary_key = True)
+    name = maindb.Column(maindb.String(50),nullable = False)
+    no_of_review = maindb.Column(maindb.Integer,nullable = False)
+    review_list = maindb.Column(maindb.PickleType,nullable = False)    
 
-class User(db.Model):
+class User(maindb.Model):
     __tablename__ = "user"
-    sno = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(50),nullable=False)
-    email = db.Column(db.String(50),nullable = False)
-    city = db.Column(db.String(30),nullable = False)
-    phone = db.Column(db.String(13),nullable = True)
-class Admin(db.Model):
+    sno = maindb.Column(maindb.Integer,primary_key = True)
+    name = maindb.Column(maindb.String(50),nullable=False)
+    email = maindb.Column(maindb.String(50),nullable = False)
+    city = maindb.Column(maindb.String(30),nullable = False)
+    state=maindb.Column(maindb.String(30),nullable = False)
+    phone = maindb.Column(maindb.String(13),nullable = True) 
+
+class Admin(maindb.Model):
     __tablename__ = "admin"
-    sno = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(50),nullable=False)
-    email = db.Column(db.String(50),nullable = False)
-    password = db.Column(db.String(10),nullable = False)
+    sno = maindb.Column(maindb.Integer,primary_key = True)
+    name = maindb.Column(maindb.String(50),nullable=False)
+    username=maindb.Column(maindb.String(50),nullable=False)
+    email = maindb.Column(maindb.String(50),nullable = False)
+    password = maindb.Column(maindb.String(10),nullable = False)
