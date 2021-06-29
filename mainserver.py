0df75f437034f21bb685c5a28300066dde2f3c1a
chaudharykapil <chaudharykapil8279@gmail.com>
2021-06-29 22:26:13 +0530

    add edit profile endpoint

--- mainserver.py
+++ mainserver.py
@@ -1,12 +1,13 @@
-from flask import Flask,render_template,request,session
-from flask.helpers import url_for
-from flask_sqlalchemy import SQLAlchemy
+import re
+from flask import Flask,render_template,request,session,abort
+from flask.helpers import flash
+from sqlalchemy.orm import selectin_polymorphic
 from werkzeug.utils import redirect
-from Database.databases import ServiceProvider,Admin,User,Ratings,Reviews,maindb as db
+from Database.databases import ServiceProvider,Admin,User,Ratings,Reviews,maindb as db 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "dfssdafasbsjcsdflhnvvbhflssdfhjsffrdees"
-#app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:kapil@localhost/service_provider_website"
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/service_provider_website"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:kapil@localhost/service_provider_website"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/service_provider_website"
 db.__init__(app)
 @app.route("/")
 def home():
@@ -26,7 +27,7 @@ def login():
         user = ServiceProvider.query.filter_by(email = email).first()
         if user:
             if user.password == password:
-                session['user'] = user.name
+                session['user'] = {"name":user.name,"email":user.email}
                 return redirect("/")
         return render_template("login_signup.html")
 
@@ -64,8 +65,33 @@ def logout():
         pass
     return redirect("/")
 
+@app.route('/profile/<string:email>')
+def profile(email):
+    user  = ServiceProvider.query.filter_by(email = email).first()
+    if user:
+        return render_template('profile.html',user = user)
+    return abort(404)
 
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
+@app.route("/profile/edit",methods = ["GET",'POST'])
+def ProfileEdit():
+    if 'user' not in session:
+        return abort(404)
+    user = ServiceProvider.query.filter_by(email = session['user']['email']).first()
+    if request.method == "GET":
+        return render_template("editProfile.html",user = user)
+    if request.method == "POST":
+        user.name = request.form.get("name")
+        user.phone = request.form.get("phone")
+        user.address = request.form.get("address")
+        user.profession = request.form.get("profession")
+        user.category = request.form.get("category")
+        user.state = request.form.get("state")
+        user.city = request.form.get("city")
+        user.speciality = request.form.get("speciality")
+        user.experience = request.form.get("experience")
+        user.qualification = request.form.get("qualification")
+        db.session.commit()
+        flash("Profile Update")
+        return redirect(f"/profile/{user.email}")       
+
 app.run(debug=True)
